bbe9ee7855ab8845e9ac2c6b1f3d10d1f9ed993a
elliotjwarner <ewarner8@uwo.ca>
2020-11-10 09:47:40 -0500

    started homepage, added components search, results, timetable

--- lab4-angular/src/app/app.module.ts
+++ lab4-angular/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SearchOptionsComponent } from './search-options/search-options.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { TimeTablesComponent } from './time-tables/time-tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchOptionsComponent,
+    SearchResultsComponent,
+    TimeTablesComponent
   ],
   imports: [
     BrowserModule
